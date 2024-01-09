f2c002febdf7f896d4fcf2299cdf17435a27021e
David Vazquez Almenara - ADAits <120119686+Dvazalm@users.noreply.github.com>
2024-01-09 17:36:53 +0100

    WebSocket funcional

---
+++ Trimestre-2/24-Websockets/src/index.js
@@ -0,0 +1,22 @@
+import app from './app.js';
+import config from './config.js';
+import { Server } from 'socket.io';
+
+
+const { port } = config;
+
+const server = app.listen(port, () => {
+    console.log(`Server listen from ${port}`)
+});
+
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    socket.emit(`hello`, `holita ❤`);
+    io.emit(`all`, `holita a todos guapetones ❤`);
+    console.log(`A user has connected`);
+    socket.on(`disconnect`,() =>{
+        console.log(`A user has disconnected`);
+    });
+});
