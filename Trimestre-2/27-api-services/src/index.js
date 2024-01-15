bfd175325cb810ed8cc3459f576d3b728426c447
David Vazquez Almenara - ADAits <120119686+Dvazalm@users.noreply.github.com>
2024-01-15 19:56:37 +0100

    ya da las peticiones

--- Trimestre-2/27-api-services/src/index.js
+++ Trimestre-2/27-api-services/src/index.js
@@ -1,6 +1,45 @@
 import app from './app.js';
 import config from './config.js';
-import { Server } from 'socket.io';
+import { getPosts, getPostsId, getCommentsFromPost } from './services/jph-service.js';
+
+
+app.get('/posts', async (req, res) => {
+    try {
+        const { filter, order, page } = req.query;
+        const posts = await getPosts({ filter, order, page });
+        res.json(posts);
+        console.log(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/posts/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const post = await getPostsId(postId);
+        res.json(post);
+        console.log(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/posts/:postId/comments', async (req, res) => {
+    try {
+        const postId = req.params.postId;
+        const comments = await getCommentsFromPost(postId);
+        res.json(comments);
+        console.log(comments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 
 
 const { port } = config;
@@ -9,6 +48,4 @@ const server = app.listen(port, () => {
     console.log(`Server listen from ${port}`)
 });
 
-import { getPosts } from './services/jph-service.js';
-const posts = await getPosts();
-console,log(posts);
+
