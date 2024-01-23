3a7ef62927af123a498973e2d8c9b6c3a3402037
David Vazquez Almenara - ADAits <120119686+Dvazalm@users.noreply.github.com>
2024-01-23 19:30:41 +0100

    upadte 33

--- Trimestre-2/33-mongoose/src/routes/Animal-router.js
+++ Trimestre-2/33-mongoose/src/routes/Animal-router.js
@@ -6,5 +6,5 @@ const router = Router();
 router.get('/', getAnimals);
 router.post('/', createAnimal);
 router.patch('/:id', updateAnimal);
-router.delete('/delete/:id', deleteAnimal);
+router.delete('/:id', deleteAnimal);
 export default router;
